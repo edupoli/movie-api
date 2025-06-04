00fb5f8166be08cc2dcaf29a49e2157a6f3d3d7b
Eduardo Policarpo <eduardo@eduardos-ipro.poli.local>
2025-06-04 20:22:20 -0300

    fix: add envs relacionadas a banco de dados

--- src/inserir.ts
+++ src/inserir.ts
@@ -16,11 +16,11 @@ interface DbConfig {
 }
 
 const DB_CONFIG: DbConfig = {
-  host: "localhost",
-  database: "cinemas",
-  user: "postgres",
-  password: "postgres",
-  port: "5432",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "cinemas",
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  port: process.env.DB_PORT || "5432",
 };
 
 // Initialize OpenAI client
